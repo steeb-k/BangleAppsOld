66a18dce74aee3761dbeea6a928a3954db9dd1e3
steeb-k <133536278+steeb-k@users.noreply.github.com>
2025-05-26 14:57:22 -0700

    a

--- apps/gnome-clock/app.js
+++ apps/gnome-clock/app.js
@@ -4,23 +4,20 @@ var settings = require("Storage").readJSON("settings.json", 1) || {
 };
 
 const lockIcon = {
-  width: 12, height: 12, bpp: 1,
+  width: 8, height: 8, bpp: 1,
   transparent: 0,
   buffer: E.toUint8Array([
-    0b00011110, 0b00000000,
-    0b00100001, 0b00000000,
-    0b01000000, 0b00000000,
-    0b10011111, 0b00000000,
-    0b10100001, 0b00000000,
-    0b10100001, 0b00000000,
-    0b10100001, 0b00000000,
-    0b10100001, 0b00000000,
-    0b10111111, 0b00000000,
-    0b10100001, 0b00000000,
-    0b10100001, 0b00000000,
-    0b11111111, 0b00000000
+    0b00111100,
+    0b01000010,
+    0b10011001,
+    0b10000001,
+    0b11111111,
+    0b11111111,
+    0b10000001,
+    0b11111111
   ])
 };
+
 
 function drawLockIcon() {
   if (settings.showLockIconWhenLocked && Bangle.isLocked()) {
